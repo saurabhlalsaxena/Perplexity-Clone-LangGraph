05f94864ce68d166bde8de5db3da4fb97e32d330
saurabhlalsaxena <saurabhlal193@gmail.com>
2024-08-22 16:54:11 +0530

    Update CORSmiddleware

--- app/server.py
+++ app/server.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 from langserve import add_routes
 from .router import router
@@ -17,6 +18,16 @@ app = FastAPI(
     description="First version of perplexity clone",
     version="0.0.1"
 )
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 async def redirect_root_to_docs():
